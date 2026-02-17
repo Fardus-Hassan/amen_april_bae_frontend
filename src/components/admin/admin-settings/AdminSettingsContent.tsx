ed22db234fd1fcf23fc37b60b71162dc316a28f6
engrnahiankhan <engrnahiankhan@gmail.com>
2026-02-17 16:04:17 +0600

    admin settings initial page setup done

---
+++ src/components/admin/admin-settings/AdminSettingsContent.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import PageTitle from "@/components/shared/PageTitle";
+
+export default function AdminSettingsContent() {
+  return (
+    <div>
+      <PageTitle text="Admin Settings" />
+    </div>
+  );
+}
