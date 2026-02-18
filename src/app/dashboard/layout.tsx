530a3292b20b7fa67b1d0c82a3f6c908b6d9ba5f
engrnahiankhan <engrnahiankhan@gmail.com>
2026-02-18 14:39:10 +0600

    my stories page done

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,11 @@
 import DashboardLayout from "@/components/layouts/DashboardLayout";
-import React from "react";
+import React, { Suspense } from "react";
 import "@/styles/dashboardStyle.css";
 
 export default function layout({ children }: { children: React.ReactNode }) {
-  return <DashboardLayout>{children}</DashboardLayout>;
+  return (
+    <DashboardLayout>
+      <Suspense>{children}</Suspense>
+    </DashboardLayout>
+  );
 }
