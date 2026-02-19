33d74799a3cd1e31d2dfc422500c6b7e7446c9a7
engrnahiankhan <engrnahiankhan@gmail.com>
2026-02-19 15:12:58 +0600

    navbar updated

--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
 import { motion } from "framer-motion";
 import { BellIcon, ProfileIcon, MenuIcon } from "@/components/icons";
 import {
