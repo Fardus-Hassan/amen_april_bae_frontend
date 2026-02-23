8bd15e56a4714a93c56c336e7c9d5a95c8d9c37e
engrnahiankhan <engrnahiankhan@gmail.com>
2026-02-23 10:57:35 +0600

    footer done

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/landing/Footer";
 import { Navbar } from "@/components/landing/Navbar";
 import "@/styles/globals.css";
 
@@ -10,6 +11,7 @@ export default function LandingLayout({
     <div className="min-h-screen bg-landing-bg">
       <Navbar />
       <main className="pt-[50px] lg:pt-[81px]">{children}</main>
+      <Footer />
     </div>
   );
 }
