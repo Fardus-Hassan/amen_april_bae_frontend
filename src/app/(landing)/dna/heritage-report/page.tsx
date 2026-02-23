f77db982ba07835e68cb27350b97c882727d3a82
engrnahiankhan <engrnahiankhan@gmail.com>
2026-02-23 08:50:28 +0600

    dna step updated

---
+++ src/app/(landing)/dna/heritage-report/page.tsx
@@ -0,0 +1,5 @@
+import HeritageReportContent from "@/components/dna/heritage-report/HeritageReportContent";
+
+export default function page() {
+  return <HeritageReportContent />;
+}
---
+++ src/components/dna/heritage-report/HeritageReportContent.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function HeritageReportContent() {
+  return <div>HeritageReportContent page component</div>;
+}
